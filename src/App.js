eb31d280988ee8064bba569a098fe6b2395319bd
SamuelRiveraB <114023422+SamuelRiveraB@users.noreply.github.com>
2022-10-10 13:18:30 -0500

    Fix last commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
+import React from "react";
 import './App.css';
-import Navigation from "./components/Navigation";
-import ImageLinkForm from "./components/ImageLinkForm";
-import ParticlesBg from 'particles-bg';
+import CardList from './CardList';
+import Header from './Header';
+import Scroll from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="square" color={["#ff0000"]}  num={20} bg={true} />
-      <Navigation name="Sam"/>
-      <ImageLinkForm />
-  {/*<FaceRecognition />*/}
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+        robots: [],
+        searchfield: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
+      return response.json();
+    }).then(users => {
+      this.setState({ robots: users });
+    });
+  }
+
+  onHandleChange = (event) => {
+    this.setState({ searchfield: event.target.value});
+  }
+
+  render() {
+    const filtered = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="App">
+          <Header handleChange={this.onHandleChange} />
+          <Scroll>
+            <CardList robots={filtered}/>
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
